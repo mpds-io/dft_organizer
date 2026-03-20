a6b2b886ccdf9ab4f51bc608a4a850eb5b729586
Evgeny Blokhin <eb@tilde.pro>
2026-03-20 17:00:10 +0100

    Fix Fleur parsing

--- dft_organizer/fleur_parser/__init__.py
+++ dft_organizer/fleur_parser/__init__.py
@@ -1,4 +1,9 @@
+import logging
+
 from .summary import parse_fleur_output
 from .error_fleur_parser import make_report, print_report, save_report
 
 __all__ = ["parse_fleur_output", "make_report", "print_report", "save_report"]
+
+fleur_logger = logging.getLogger("masci_tools")
+fleur_logger.propagate = False
